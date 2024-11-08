085fbf901df74db8f2fdf8cb4951e44207f3c4f2
TamimOp <tamimshadanik@gmail.com>
2024-11-08 14:51:17 +0600

    header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import Header from "./components/Header";
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Wellcome to Tasker!</h1>
+      <Header />
+    </>
+  );
 }
