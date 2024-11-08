3a4d5b97955d1ca705721d0a436704c9df640b78
TamimOp <tamimshadanik@gmail.com>
2024-11-08 22:28:30 +0600

    added task components and structured the task section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import TaskBoard from "./components/task/TaskBoard";
 export default function App() {
   return (
     <>
       <Header />
-      <Hero />
+      <div className="flex flex-col justify-center items-center">
+        <Hero />
+        <TaskBoard />
+      </div>
       <Footer />
     </>
   );
