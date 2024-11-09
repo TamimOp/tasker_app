bf3fa93d050200e271220a95e6618cff8604a881
TamimOp <tamimshadanik@gmail.com>
2024-11-09 18:16:08 +0600

    making the taskboard dynamic

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
-import TaskList from "./TaskLIst";
+import TaskLists from "./TaskLists";
 
 function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learning React",
+    description: "Learning like this really helpful and clear for a developer",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavourite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -11,7 +22,7 @@ function TaskBoard() {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
-          <TaskList />
+          <TaskLists tasks={tasks} />
         </div>
       </div>
     </section>
