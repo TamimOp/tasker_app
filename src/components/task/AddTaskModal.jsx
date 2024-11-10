ba20bf641f9ddd6dc5d02037b20cfb31ba617643
TamimOp <tamimshadanik@gmail.com>
2024-11-10 21:43:08 +0600

    created edit logic

--- src/components/task/AddTaskModal.jsx
+++ src/components/task/AddTaskModal.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 
-function AddTaskModal({ onSave }) {
-  const [newTask, setNewTask] = useState({
-    id: crypto.randomUUID(),
-    title: "",
-    description: "",
-    tags: [],
-    priority: "",
-    isFavourite: false,
-  });
+function AddTaskModal({ onSave, taskToUpdate }) {
+  const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null));
+  const [task, setTask] = useState(
+    taskToUpdate || {
+      id: crypto.randomUUID(),
+      title: "",
+      description: "",
+      tags: [],
+      priority: "",
+      isFavourite: false,
+    }
+  );
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -16,17 +19,18 @@ function AddTaskModal({ onSave }) {
     if (name === "tags") {
       value = value.split(",");
     }
-    setNewTask({
-      ...newTask,
+    setTask({
+      ...task,
       [name]: value,
     });
   };
+
   return (
     <>
       <div className="fixed inset-0 bg-black bg-opacity-70 z-10"></div>
       <form className="fixed z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:p-11">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-          Add New Task
+          {isAdd ? "Add New Task" : "Edit Task"}
         </h2>
 
         <div className="space-y-9 text-white lg:space-y-10">
@@ -36,7 +40,7 @@ function AddTaskModal({ onSave }) {
               className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
               type="text"
               name="title"
-              value={newTask.title}
+              value={task.title}
               onChange={handleChange}
               id="title"
               required
@@ -48,7 +52,7 @@ function AddTaskModal({ onSave }) {
               className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
               type="text"
               name="description"
-              value={newTask.description}
+              value={task.description}
               onChange={handleChange}
               id="description"
               required
@@ -61,7 +65,7 @@ function AddTaskModal({ onSave }) {
                 className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
                 type="text"
                 name="tags"
-                value={newTask.tags}
+                value={task.tags}
                 onChange={handleChange}
                 id="tags"
                 required
@@ -72,7 +76,7 @@ function AddTaskModal({ onSave }) {
               <select
                 className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
                 name="priority"
-                value={newTask.priority}
+                value={task.priority}
                 onChange={handleChange}
                 id="priority"
                 required
@@ -89,9 +93,9 @@ function AddTaskModal({ onSave }) {
           <button
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
-            onClick={() => onSave(newTask)}
+            onClick={() => onSave(task, isAdd)}
           >
-            Save
+            {isAdd ? "Create Task" : "Update Task"}
           </button>
         </div>
       </form>
